96e77ca32089b7f7b657cf59b084c95c53c85e2f
Rade58 <bajic.rade2@gmail.com>
2022-08-11 00:48:58 +0200

    🦉

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-teal-600">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
