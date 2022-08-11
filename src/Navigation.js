f635bd7ff976a3bead075466e695d4b163247669
Rade58 <bajic.rade2@gmail.com>
2022-08-11 02:38:46 +0200

    🦉

---
+++ src/Navigation.js
@@ -0,0 +1,24 @@
+import {NavLink} from 'react-router-dom'
+
+
+export const Navigation = ({children}) => {
+  return <nav className='flex flex-col border-r-w border-cyan-600 h-full'>
+    {children}
+  </nav>
+}
+
+export const NavigationLink = ({children, to}) => {
+  return <NavLink
+    to={to}
+    // I GUESS YOU CAN ADD FUNCTION FOR AS A className
+    // FOR THE NavLink COMPONENT
+    className={({isActive}) => {
+      return isActive? "bg-teal-800 hover:bg-teal-100":
+      "bg-cyan-300 hover:bg-cyan-800"
+    }}
+  >
+    <article className='w-full p-4'>
+      {children}
+    </article>
+  </NavLink>
+}
