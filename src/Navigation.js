cc97590f120659feb2e690a5f65956f49a67ddae
Rade58 <bajic.rade2@gmail.com>
2022-08-12 14:10:01 +0200

    🦉

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,7 +2,7 @@ import {NavLink} from 'react-router-dom'
 
 
 export const Navigation = ({children}) => {
-  return <nav className='col-span-3 flex flex-col border-r-w border-cyan-600 h-full'>
+  return <nav className='col-span-3 flex flex-col border-r-w border-stone-600 h-full'>
     {children}
   </nav>
 }
@@ -15,8 +15,8 @@ export const NavigationLink = ({children, to}) => {
     // HEWRE isActive MEANS THAT IT IS GOING TO BE true
     // WHEN ROUTE IS CURRENTLY VISITED
     className={({isActive}) => {
-      return isActive? "bg-teal-900 hover:bg-teal-400":
-      "bg-stone-700 hover:bg-stone-600"
+      return isActive? "bg-stone-900 hover:bg-stone-400":
+      "bg-indigo-700 hover:bg-indigo-600"
     }}
   >
     <article className='w-full p-4'>
